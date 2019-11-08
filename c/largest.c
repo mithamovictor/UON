412bfb0ebae79ddfec3895455b0840c78801684d
orrasis <orrasis@localhost.localdomain>
2019-11-08 11:37:36 +0300

    updated function

--- c/largest.c
+++ c/largest.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 void main() {
-    int arr[20], i;
-
-    for ( i = 0; i < 20; i++ ) {
-        printf("Enter number at the %d position", i+1 );
+    
+    int arr[100], max, n, i, loc = 1;
+    
+    printf("Enter the number of elements in array\n");
+    scanf("%d", &n);
+    
+    printf("Enter %d integers\n", n);
+    
+    for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
-        printf("%d\n", arr[]);
+    
+    max = arr[0];
+    
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+        max  = arr[i];
+        loc = i+1;
+        }
     }
+    
+    printf("Maximum element is present at location %d and it's value is %d.\n", loc, max);
 }
